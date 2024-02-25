9beb93982907bd4cc49e4087d73c104fe98da49a
Tetsu-is <hap30305@kwansei.ac.jp>
2024-02-26 01:13:55 +0900

    test code done

--- test/api_test.go
+++ test/api_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"encoding/base64"
-	"fmt"
 	"net/http"
 	"os"
 	"testing"
@@ -20,20 +19,19 @@ import (
 // 空の User ID, Password を送信した場合、 Basic 認証が失敗し HTTP Status Code が 401 で返却されているかどうか。
 // アクセス時に User ID, Password を送信しなかった場合、Basic 認証が失敗し HTTP Status Code が 401 で返却されているかどうか。
 
+const baseUrl = "http://localhost:8080"
+
 func Test1(t *testing.T) {
+	// 対象の API のみ Basic 認証がかかっているか、どうか。
 	t.Parallel()
 
 	if err := godotenv.Load("../.env"); err != nil {
 		t.Errorf("Error loading .env file")
 	}
 
-	const baseUrl = "http://localhost:8080"
-
 	testcases := map[string]struct {
 		Endpoints    string
 		RequestType  string
-		UserID       string
-		Password     string
 		ExpectStatus int
 	}{
 
@@ -60,12 +58,57 @@ func Test1(t *testing.T) {
 			RequestType:  "GET",
 			ExpectStatus: 401,
 		},
+	}
 
+	for name, tc := range testcases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			url := baseUrl + tc.Endpoints
+
+			req, err := http.NewRequest(tc.RequestType, url, nil)
+			if err != nil {
+				t.Errorf("Error creating request: %v", err)
+			}
+
+			client := new(http.Client)
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Errorf("Error sending request: %v", err)
+			}
+
+			if resp.StatusCode != tc.ExpectStatus {
+				t.Errorf("Expected status code %d, but got %d", tc.ExpectStatus, resp.StatusCode)
+			}
+
+			defer resp.Body.Close()
+		})
+	}
+
+}
+
+func Test2(t *testing.T) {
+	// 正しい User ID, Password で Basic 認証をクリアしアクセスできるかどうか。
+	t.Parallel()
+
+	if err := godotenv.Load("../.env"); err != nil {
+		t.Errorf("Error loading .env file")
+	}
+
+	userID := os.Getenv("BASIC_AUTH_USER_ID")
+	password := os.Getenv("BASIC_AUTH_PASSWORD")
+
+	testcases := map[string]struct {
+		Endpoints    string
+		RequestType  string
+		UserID       string
+		Password     string
+		ExpectStatus int
+	}{
 		"Auth Success (CorrectUserID_CorrectPassword)": {
 			Endpoints:    "/test",
 			RequestType:  "GET",
-			UserID:       os.Getenv("BASIC_AUTH_USER_ID"),
-			Password:     os.Getenv("BASIC_AUTH_PASSWORD"),
+			UserID:       userID,
+			Password:     password,
 			ExpectStatus: 200,
 		},
 
@@ -73,14 +116,14 @@ func Test1(t *testing.T) {
 			Endpoints:    "/test",
 			RequestType:  "GET",
 			UserID:       "wrongUserID",
-			Password:     os.Getenv("BASIC_AUTH_PASSWORD"),
+			Password:     password,
 			ExpectStatus: 401,
 		},
 
 		"Auth Fail (CorrectUserID_WrongPassword)": {
 			Endpoints:    "/test",
 			RequestType:  "GET",
-			UserID:       os.Getenv("BASIC_AUTH_USER_ID"),
+			UserID:       userID,
 			Password:     "wrongPassword",
 			ExpectStatus: 401,
 		},
@@ -106,15 +149,15 @@ func Test1(t *testing.T) {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			url := baseUrl + tc.Endpoints
-			//base64 conversion of userID and password
-			encoded := base64.StdEncoding.EncodeToString([]byte(tc.UserID + ":" + tc.Password))
 
 			req, err := http.NewRequest(tc.RequestType, url, nil)
 			if err != nil {
 				t.Errorf("Error creating request: %v", err)
 			}
 
-			req.Header.Set("Authorization", "Basic "+encoded)
+			base64Auth := base64.StdEncoding.EncodeToString([]byte(tc.UserID + ":" + tc.Password))
+
+			req.Header.Set("Authorization", "Basic "+base64Auth)
 
 			client := new(http.Client)
 			resp, err := client.Do(req)
@@ -123,12 +166,10 @@ func Test1(t *testing.T) {
 			}
 
 			if resp.StatusCode != tc.ExpectStatus {
-				fmt.Println("tried ID: ", tc.UserID, "tried Password: ", tc.Password, "tried URL: ", url, "tried RequestType: ", tc.RequestType, "tried ExpectStatus: ", tc.ExpectStatus, "got status: ", resp.StatusCode)
 				t.Errorf("Expected status code %d, but got %d", tc.ExpectStatus, resp.StatusCode)
 			}
 
 			defer resp.Body.Close()
 		})
 	}
-
 }
