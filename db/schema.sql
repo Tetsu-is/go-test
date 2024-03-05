501930ab59c59be22abfed94e62132c286177a54
Tetsu-is <hap30305@kwansei.ac.jp>
2024-03-06 02:24:00 +0900

    add migrate for users table

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,19 @@ CREATE TRIGGER IF NOT EXISTS trigger_todos_updated_at AFTER UPDATE ON todos
 BEGIN
   UPDATE todos SET updated_at = DATETIME('now') WHERE id == NEW.id;
 END;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT (DATETIME('now')),
+  updated_at DATETIME NOT NULL DEFAULT (DATETIME('now')),
+  CHECK(user_name <> '' AND email <> '' AND password <> '')
+);
+
+CREATE TRIGGER IF NOT EXISTS trigger_users_updated_at AFTER UPDATE ON users
+BEGIN
+  UPDATE users SET updated_at = DATETIME('now') WHERE id == NEW.id;
+END;
+
