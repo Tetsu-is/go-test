c4950760b4773e6dec58753284dda0d72f6c887a
Tetsu-is <hap30305@kwansei.ac.jp>
2024-03-11 18:18:21 +0900

    impl jwt authentication with jwt-go

--- handler/middleware/valid_token.go
+++ handler/middleware/valid_token.go
@@ -4,7 +4,6 @@ import (
 	"api/logic"
 	"context"
 	"net/http"
-	"time"
 )
 
 func ValidToken(h http.Handler) http.Handler {
@@ -17,23 +16,12 @@ func ValidToken(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("token")
 		if err != nil {
+			http.Error(w, "unauthorized!", http.StatusUnauthorized)
+			return
+		}
+
+		if err := logic.VerifyJwtToken(cookie.Value); err != nil {
 			http.Error(w, "unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		header, payload, err := logic.ResolveJwtToken(cookie.Value)
-		if err != nil {
-			http.Error(w, "unauthorized", http.StatusInternalServerError)
-			return
-		}
-
-		if header.Alg != "HS256" || header.Typ != "JWT" {
-			http.Error(w, "header is invalid", http.StatusUnauthorized)
-			return
-		}
-
-		if payload.Exp.Compare(time.Now()) < 0 {
-			http.Error(w, "token is expired", http.StatusUnauthorized)
 			return
 		}
 
